a0a9743d804a52c5784a000e28119d98adbed5a0
jathinreddy4235 <68458978+jathinreddy4235@users.noreply.github.com>
2023-06-22 11:10:26 +0530

    Update SearchBox.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const SearchBox = ({searchChange}) => {
     return (
-    <div className="p-2 bg-blue-400"> 
-    <input className="border-2 border-black text-3xl text-center bg-green-300 rounder-full" type = 'search' placeholder="search"
+    <div className="bg-blue-400"> 
+    <input className="w-40 sm:w-auto border-2 border-black text-3xl text-center bg-green-300 rounder-full" type = 'search' placeholder="search"
      onChange = {searchChange}/></div>
     
 );}
